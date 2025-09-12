76536a26f754048a0b98e7d70e65261bb27b1813
pero4l <lilpero03@gmail.com>
2025-09-12 23:13:06 +0100

    I am still working

---
+++ app/components/layout/footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+            <div>
+                <MdKeyboardDoubleArrowUp/>
+                <p>Back TO TOP</p>
+            </div>
+
+            <div>
+                
+            </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ app/components/layout/navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '@/public/logo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        {/* Logo */}
+            <div>
+                <Image className='w-44' src={logo} alt='Logo' />
+            </div>
+
+            <div>
+                <ul>
+                    <li>About me</li>
+                    <li>Skills</li>
+                    <li>Portfolio</li>
+
+                    <button>CONTACT ME</button>
+                </ul>
+            </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
