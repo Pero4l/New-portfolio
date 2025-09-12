db71957a4f45df0c8d14b87f1556b12467482899
pero4l <lilpero03@gmail.com>
2025-09-12 23:28:29 +0100

    i just did the footer

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React from "react";
 import { MdKeyboardDoubleArrowUp } from "react-icons/md";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaSquareGithub } from "react-icons/fa6";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebookSquare, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
+import { FaSquareGithub, FaSquareXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>
-      <footer>
-            <div>
-                <MdKeyboardDoubleArrowUp/>
-                <p>Back TO TOP</p>
-            </div>
+    <footer className="bg-black text-white py-6 text-center">
+      <div className="flex flex-col items-center gap-3">
+        {/* Back to Top */}
+        <div className="flex flex-col  items-center gap-2 cursor-pointer hover:text-blue-400">
+          <MdKeyboardDoubleArrowUp size={20} />
+          <p className="font-semibold">Back To Top</p>
+        </div>
 
-            <div>
+        {/* Social Icons */}
+        <div className="flex gap-4 text-3xl">
+          <FaFacebookSquare className="cursor-pointer hover:text-blue-500" />
+          <FaLinkedin className="cursor-pointer hover:text-blue-700" />
+          <FaSquareGithub className="cursor-pointer hover:text-gray-400" />
+          <FaInstagramSquare className="cursor-pointer hover:text-pink-500" />
+          <FaSquareXTwitter className="cursor-pointer hover:text-sky-400" />
+        </div>
 
-            </div>
-      </footer>
-    </div>
-  )
-}
+        {/* Copyright */}
+        <div className="">
+          <p><span className="font-semibold">&copy;{new Date().getFullYear()} PETER IDIKU</span> All rights reserved. </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
