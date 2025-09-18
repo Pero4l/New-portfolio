ffdf0a04eb60bef61c160e56c4f01faa2f54f7ba
pero4l <lilpero03@gmail.com>
2025-09-18 22:47:16 +0100

    added link to the footer icon

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { MdKeyboardDoubleArrowUp } from "react-icons/md";
-import { FaFacebookSquare, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
+import { FaFacebookSquare, FaLinkedin, FaInstagramSquare, FaYoutube } from "react-icons/fa";
 import { FaSquareGithub, FaSquareXTwitter } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io";
 import Link from "next/link";
 
 const Footer = () => {
@@ -15,12 +16,30 @@ const Footer = () => {
         </Link>
 
         {/* Social Icons */}
-        <div className="flex gap-4 text-3xl">
-          <FaFacebookSquare className="cursor-pointer hover:text-blue-500" />
-          <FaLinkedin className="cursor-pointer hover:text-blue-700" />
-          <FaSquareGithub className="cursor-pointer hover:text-gray-400" />
-          <FaInstagramSquare className="cursor-pointer hover:text-pink-500" />
-          <FaSquareXTwitter className="cursor-pointer hover:text-sky-400" />
+        <div className="flex gap-4 text-4xl lg:text-5xl pt-5">
+          <Link href="https://www.facebook.com/Dev.Ptb?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer">
+            <FaFacebookSquare className="cursor-pointer hover:text-blue-600" />
+          </Link>
+
+          <Link href="https://www.linkedin.com/in/peter-idiku-911178338?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="cursor-pointer hover:text-blue-700" />
+          </Link>
+
+          <Link href="https://github.com/Pero4l" target="blank" rel="noopener noreferrer">
+            <FaSquareGithub className="cursor-pointer hover:text-gray-400" />
+          </Link>
+
+          <Link href="https://www.instagram.com/pero4l/" target="_blank" rel="noopener noreferrer">
+            <FaInstagramSquare className="cursor-pointer hover:text-pink-500" />
+          </Link>
+
+          <Link href="https://x.com/pero4l?s=21">
+          <FaSquareXTwitter className="cursor-pointer hover:text-gray-900" />
+          </Link>
+
+          <Link href="https://www.youtube.com/@Pero_media1">
+          <IoLogoYoutube className="cursor-pointer hover:text-red-500" />
+          </Link>
         </div>
 
         {/* Copyright */}
